19f48c76f6ba6c8257ded7b6a6fcfb5f1f5eb824
Matias Espina <matiasespina1991@gmail.com>
2026-04-29 11:27:11 +0200

    fix build

--- apps/admin-dashboard/src/proxy.ts
+++ apps/admin-dashboard/src/proxy.ts
@@ -7,6 +7,6 @@ export default function proxy(_request: NextRequest) {
 
 export const config = {
   matcher: [
-    String.raw`/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)`
+    '/((?!_next|[^?]*[.](?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)'
   ]
 };
